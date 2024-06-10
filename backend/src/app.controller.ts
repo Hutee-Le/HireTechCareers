a75748093eed585b48f0359f3db37ab1c8ff4f61
Minh Nhut <lenhut0929@gmail.com>
2024-06-11 03:02:22 +0700

    nest update JWT token 1th

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Render,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { Public } from './decorator/customize';
 
 @Controller() // route /
 export class AppController {
@@ -26,17 +17,5 @@ export class AppController {
     return {
       message: message,
     };
-  }
-
-  @Public()
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async handleLogin(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
   }
 }
