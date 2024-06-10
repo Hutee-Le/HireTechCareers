911bf7e536aca8084ec1017fe4754d66672150f4
Minh Nhut <lenhut0929@gmail.com>
2024-06-11 02:30:17 +0700

    update companies module 1th

--- backend/src/companies/companies.module.ts
+++ backend/src/companies/companies.module.ts
@@ -6,10 +6,9 @@ import { Company, CompanySchema } from './schemas/company.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Company.name, schema: CompanySchema}])
+    MongooseModule.forFeature([{ name: Company.name, schema: CompanySchema }]),
   ],
   controllers: [CompaniesController],
   providers: [CompaniesService],
-  // exports [CompaniesService],
 })
 export class CompaniesModule {}
